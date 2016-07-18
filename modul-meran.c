24063aba7d9d43e83b00a23599f2d6e2c9439685
merandrei0508 <merandrei0508@gmail.com>
2016-07-18 16:05:53 +0300

    modul-meran.c

--- modul-meran.c
+++ modul-meran.c
@@ -1,14 +1,20 @@
 #include <linux/init.h>
 #include <linux/module.h>
-MODULE_LICENSE("Dual BSD/GPL");
+
 static int hello_init(void)
 {
-    printk(KERN_ALERT "Hello, world\n");
-    return 0;
+	pr_debug("Hello, world\n");
+
+	/*
+	 * A non 0 return means init_module failed; module can't be loaded.
+	 */
+	return 0;
 }
+
 static void hello_exit(void)
 {
-    printk(KERN_ALERT "Goodbye, cruel world\n");
+	pr_debug("Goodbye, cruel world\n");
 }
+
 module_init(hello_init);
 module_exit(hello_exit);
